0c0e2af0f56b762a01f9e62af33c0692563538e5
tiagaoprates <tiagoprates_911@hotmail.com>
2018-03-05 21:12:37 -0300

    5 - Teste unitario

--- servico_juridico/tests.py
+++ servico_juridico/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from servico_juridico.model.empresa_data import Empresa
+from servico_juridico.model.ordem_servico_data import OrdemServico
 
-# Create your tests here.
+
+class PropostaTestCase(TestCase):
+    def setUp(self):
+        Empresa.objects.create(nome='Produtos Alimenticios LTDA', ramo_atuacao='Alimentos')
+        OrdemServico.objects.create(empresa_id=Empresa.id, titulo='ABC',
+                                    descricao='Teste teste teste')
+
+    def test_ordem_servico_state(self):
+        nova_ordem = OrdemServico.objects.get(titulo='ABC')
+        self.assertEqual(nova_ordem.finaliza_ordem_servico(), nova_ordem.FINALIZADA)
